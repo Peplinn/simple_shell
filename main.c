c25b050fa9ed848bb15e1f5bbcd477f7568ce947
Peplinn <chidiebubeoluoma01@gmail.com>
2023-09-20 16:42:33 +0100

    Exit argument fix

--- main.c
+++ main.c
@@ -48,7 +48,7 @@ int main(UNUSED int argc, UNUSED char **argv)
 			pid_check(child_pid, args);
 	}
 	free(input);
-	return (extenv == 1) ? exit_status : status;
+	return ((extenv == 1) ? exit_status : status);
 }
 
 /**
